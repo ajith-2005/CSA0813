c3965286d61f2dc7101b83d52326383222a23ed1
ajith-2005 <ajithchoppadandi@gmail.com>
2024-07-30 10:49:17 +0530

    Add files via upload

--- factorial using function.py
+++ factorial using function.py
@@ -2,6 +2,6 @@ def fact(n):
     fact=1
     for i in range(1,n+1):
         fact=fact*i
-        return fact
-    n=6
-    print("Factorial of {}is{}".format(n,fact(n)))
+    return fact
+n=int(input("enter the value of n:"))
+print("factorial=",fact(n))
